44cc6c2d599fbb9198c09622811f15937133ff60
Dickyasyy <dickyassidiq45@gmail.com>
2026-02-16 23:14:02 +0700

    mengganti file huruf yang tidak bisa di deploy vercel

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,15 @@
 // Import Foto 
 import Profil from "./Images/Profil.png";
-import weddingProject from "./Images/Project Wedding.png";
-import servisProject from "./Images/Web Servis.jpg";
-import InvestProject from "./Images/Aplikasi Panduan Investasi.jpeg";
+import weddingProject from "./Images/Project-Wedding.png";
+import servisProject from "./Images/Web-Servis.jpg";
+import InvestProject from "./Images/Aplikasi-Panduan-Investasi.jpeg";
 
 // Import Semua Animasi
-import AnimFly from "./Animasi/animasi orang terbang.png";
-import AnimClick from "./Animasi/animasi tekan tombol.png";
-import AnimAbout1 from "./Animasi/about 1.png";
-import AnimAbout2 from "./Animasi/about 2.png";
-import AnimProject from "./Animasi/animasi project.png";
+import AnimFly from "./Animasi/animasi-orang-terbang.png";
+import AnimClick from "./Animasi/animasi-tekan-tombol.png";
+import AnimAbout1 from "./Animasi/about-1.png";
+import AnimAbout2 from "./Animasi/about-2.png";
+import AnimProject from "./Animasi/animasi-project.png";
 
 // Export dalam satu objek besar
 export const GlobalImages = {
